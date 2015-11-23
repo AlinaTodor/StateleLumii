daf02d9d3edb0ab28a0e141b3c4e13e5cec1ea4d
deliaserbanescu <andreeadeliaserbanescu94@gmail.com>
2015-11-23 22:41:21 +0200

    a

--- app/src/main/java/com/statelelumii/Inregistrare.java
+++ app/src/main/java/com/statelelumii/Inregistrare.java
@@ -6,14 +6,32 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class Inregistrare extends AppCompatActivity {
+public class Inregistrare extends AppCompatActivity  implements View.OnClickListener{
 
+    EditText etNume, etEmail, etUSER, etParola, etREP;
+    Button bInregistrare;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inregistrare);
 
+        etNume = (EditText)findViewById(R.id.etNume);
+        etEmail = (EditText)findViewById(R.id.etEmail);
+        etUSER = (EditText)findViewById(R.id.etUsername);
+        etParola = (EditText)findViewById(R.id.etParola);
+        etREP = (EditText)findViewById(R.id.etREP);
+        bInregistrare = (Button)findViewById(R.id.bInregistrare);
+
+        bInregistrare.setOnClickListener(this);
+
     }
 
+    @Override
+    public void onClick(View v) {
+
+    }
 }
+
